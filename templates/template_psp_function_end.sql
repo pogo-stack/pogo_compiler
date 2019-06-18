84633d4e2546c548c6845132d3ae7d1fac843247
Evgeny <evgeny@komarevtsev.net>
2019-06-18 23:45:59 +1000

    updating noauth instead of inserting for existing files

--- templates/template_psp_function_end.sql
+++ templates/template_psp_function_end.sql
@@ -89,8 +89,9 @@ select distinct '$name$', unnest(regexp_split_to_array($dependencies$, ','));
 
 do $$
 begin
-	insert into __pogo__compiled_source (name, latest_version, is_noauth)
-	values ('$name$', 1, $is_noauth$);
+	insert into __pogo__compiled_source (name, latest_version)
+	values ('$name$', 1);
+	update __pogo__compiled_source set is_auth = $is_noauth$ where name = '$name$';
 exception when others then
 	update __pogo__compiled_source
 	set latest_version = latest_version + 1
